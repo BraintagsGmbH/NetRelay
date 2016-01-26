7c2122b4ea21e897af530b40c93cb696fe4abf4a
remi128 <mremme@braintags.de>
2016-01-26 12:18:35 +0100

    AbstractRecord

---
+++ src/main/java/de/braintags/netrelay/model/AbstractRecord.java
@@ -0,0 +1,39 @@
+/*
+ * #%L
+ * netrelay
+ * %%
+ * Copyright (C) 2015 Braintags GmbH
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
+package de.braintags.netrelay.model;
+
+import java.sql.Timestamp;
+
+import de.braintags.io.vertx.pojomapper.annotation.Entity;
+import de.braintags.io.vertx.pojomapper.annotation.field.Id;
+import de.braintags.io.vertx.pojomapper.annotation.lifecycle.BeforeSave;
+
+/**
+ * Abstract implementation for a record, which takes care about modification date and other basic information
+ * 
+ * @author Michael Remme
+ * 
+ */
+@Entity
+public abstract class AbstractRecord {
+  @Id
+  public String id;
+  public Timestamp createdOn = new Timestamp(System.currentTimeMillis());
+  public Timestamp modifiedOn;
+
+  @BeforeSave
+  public void beforeSave() {
+    modifiedOn = new Timestamp(System.currentTimeMillis());
+  }
+
+}
