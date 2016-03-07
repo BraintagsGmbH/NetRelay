86872537529360abc1ce5483e9f872b180789d7a
remi128 <mremme@braintags.de>
2016-03-07 12:42:06 +0100

    created and used AbstractAuthProviderController

---
+++ src/main/java/de/braintags/netrelay/controller/impl/authentication/AbstractAuthProviderController.java
@@ -0,0 +1,170 @@
+/*
+ * #%L
+ * vertx-pojongo
+ * %%
+ * Copyright (C) 2015 Braintags GmbH
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
+package de.braintags.netrelay.controller.impl.authentication;
+
+import java.util.Properties;
+
+import de.braintags.io.vertx.pojomapper.IDataStore;
+import de.braintags.io.vertx.pojomapper.mongo.MongoDataStore;
+import de.braintags.netrelay.controller.impl.AbstractController;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.AuthProvider;
+import io.vertx.ext.auth.User;
+import io.vertx.ext.auth.mongo.HashSaltStyle;
+import io.vertx.ext.auth.mongo.MongoAuth;
+
+/**
+ * An abstract implementation of IController, which initializes an {@link AuthProvider} to be used to perform
+ * authentication and authorization in extending controllers
+ * 
+ * Config-Parameter:<br/>
+ * possible parameters, which are read from the configuration
+ * <UL>
+ * <LI>parameter1 - describe the sense of the parameter
+ * <LI>parameter2 - describe the sense of the parameter
+ * </UL>
+ * <br>
+ * 
+ * Request-Parameter:<br/>
+ * possible parameters, which are read from a request
+ * <UL>
+ * <LI>parameter1 - describe the sense of the parameter
+ * <LI>parameter2 - describe the sense of the parameter
+ * </UL>
+ * <br/>
+ * 
+ * Result-Parameter:<br/>
+ * possible paramters, which will be placed into the context
+ * <UL>
+ * <LI>parameter1 - describe the content, which is stored under the given parameter name
+ * </UL>
+ * <br/>
+ * 
+ * 
+ * @author Michael Remme
+ * 
+ */
+public abstract class AbstractAuthProviderController extends AbstractController {
+  private static final io.vertx.core.logging.Logger LOGGER = io.vertx.core.logging.LoggerFactory
+      .getLogger(AbstractAuthProviderController.class);
+
+  /**
+   * Used as possible value for property {@link #AUTH_PROVIDER_PROP} and references to an authentivation provider
+   * connected to a mongo db
+   */
+  public static final String AUTH_PROVIDER_MONGO = "MongoAuth";
+
+  /**
+   * The name of the key, which is used, to store the name of the mapper in the {@link User#principal()}
+   */
+  public static final String MAPPERNAME_IN_PRINCIPAL = "mapper";
+
+  /**
+   * Defines the name of the {@link AuthProvider} to be used
+   */
+  public static final String AUTH_PROVIDER_PROP = "authProvider";
+
+  private AuthProvider authProvider;
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.braintags.netrelay.controller.impl.AbstractController#initProperties(java.util.Properties)
+   */
+  @Override
+  public void initProperties(Properties properties) {
+    this.authProvider = createAuthProvider(properties);
+  }
+
+  /**
+   * Get the initialized instance of {@link AuthProvider}
+   * 
+   * @return the {@link AuthProvider}
+   */
+  protected AuthProvider getAuthProvider() {
+    return authProvider;
+  }
+
+  private AuthProviderProxy createAuthProvider(Properties properties) {
+    String authProvider = (String) properties.get(AUTH_PROVIDER_PROP);
+    String mapper = readProperty(MongoAuth.PROPERTY_COLLECTION_NAME, null, true);
+    if (authProvider.equals(AUTH_PROVIDER_MONGO)) {
+      return new AuthProviderProxy(initMongoAuthProvider(mapper), mapper);
+    } else {
+      throw new UnsupportedOperationException("unsupported authprovider: " + authProvider);
+    }
+  }
+
+  /**
+   * Init the Authentication Service
+   */
+  private AuthProvider initMongoAuthProvider(String mapper) {
+    IDataStore store = getNetRelay().getDatastore();
+    if (!(store instanceof MongoDataStore)) {
+      throw new IllegalArgumentException("MongoAuthProvider expects a MongoDataStore");
+    }
+    JsonObject config = new JsonObject();
+    String saltStyle = readProperty(MongoAuth.PROPERTY_SALT_STYLE, HashSaltStyle.NO_SALT.toString(), false);
+    config.put(MongoAuth.PROPERTY_SALT_STYLE, HashSaltStyle.valueOf(saltStyle));
+    MongoAuth auth = MongoAuth.create(((MongoDataStore) store).getMongoClient(), config);
+
+    auth.setPasswordField(readProperty(MongoAuth.PROPERTY_PASSWORD_FIELD, null, true));
+    auth.setUsernameField(readProperty(MongoAuth.PROPERTY_USERNAME_FIELD, null, true));
+    auth.setCollectionName(mapper);
+
+    String roleField = readProperty(MongoAuth.PROPERTY_ROLE_FIELD, null, false);
+    if (roleField != null) {
+      auth.setRoleField(roleField);
+    }
+    String saltField = readProperty(MongoAuth.PROPERTY_SALT_FIELD, null, false);
+    if (saltField != null) {
+      auth.setSaltField(saltField);
+    }
+
+    return auth;
+  }
+
+  class AuthProviderProxy implements AuthProvider {
+    AuthProvider prov;
+    String mapper;
+
+    AuthProviderProxy(AuthProvider prov, String mapper) {
+      this.prov = prov;
+      this.mapper = mapper;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see io.vertx.ext.auth.AuthProvider#authenticate(io.vertx.core.json.JsonObject, io.vertx.core.Handler)
+     */
+    @Override
+    public void authenticate(JsonObject arg0, Handler<AsyncResult<User>> handler) {
+      prov.authenticate(arg0, result -> {
+        if (result.failed()) {
+          LOGGER.info("Authentication failed: " + result.cause());
+          handler.handle(result);
+        } else {
+          User user = result.result();
+          user.principal().put(MAPPERNAME_IN_PRINCIPAL, mapper);
+          handler.handle(Future.succeededFuture(user));
+        }
+      });
+    }
+
+  }
+
+}
--- src/main/java/de/braintags/netrelay/controller/impl/authentication/AuthenticationController.java
+++ src/main/java/de/braintags/netrelay/controller/impl/authentication/AuthenticationController.java
@@ -14,18 +14,9 @@ package de.braintags.netrelay.controller.impl.authentication;
 
 import java.util.Properties;
 
-import de.braintags.io.vertx.pojomapper.IDataStore;
-import de.braintags.io.vertx.pojomapper.mongo.MongoDataStore;
 import de.braintags.netrelay.RequestUtil;
-import de.braintags.netrelay.controller.impl.AbstractController;
 import de.braintags.netrelay.routing.RouterDefinition;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.AuthProvider;
-import io.vertx.ext.auth.User;
-import io.vertx.ext.auth.mongo.HashSaltStyle;
 import io.vertx.ext.auth.mongo.MongoAuth;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.AuthHandler;
@@ -65,7 +56,7 @@ import io.vertx.ext.web.handler.UserSessionHandler;
  * @author mremme
  * 
  */
-public class AuthenticationController extends AbstractController {
+public class AuthenticationController extends AbstractAuthProviderController {
   private static final io.vertx.core.logging.Logger LOGGER = io.vertx.core.logging.LoggerFactory
       .getLogger(AuthenticationController.class);
 
@@ -114,22 +105,6 @@ public class AuthenticationController extends AbstractController {
   public static final String LOGOUT_MESSAGE_PROP = "logoutMessage";
 
   /**
-   * Used as possible value for property {@link #AUTH_PROVIDER_PROP} and references to an authentivation provider
-   * connected to a mongo db
-   */
-  public static final String AUTH_PROVIDER_MONGO = "MongoAuth";
-
-  /**
-   * The name of the key, which is used, to store the name of the mapper in the {@link User#principal()}
-   */
-  public static final String MAPPERNAME_IN_PRINCIPAL = "mapper";
-
-  /**
-   * Defines the name of the {@link AuthProvider} to be used
-   */
-  public static final String AUTH_PROVIDER_PROP = "authProvider";
-
-  /**
    * Defines the name of the {@link AuthHandler} to be used
    */
   public static final String AUTH_HANDLER_PROP = "authHandler";
@@ -140,7 +115,6 @@ public class AuthenticationController extends AbstractController {
   public static final String LOGIN_PAGE_PROP = "loginPage";
 
   protected AuthHandler authHandler;
-  protected AuthProvider authProvider;
   private String loginPage;
 
   /*
@@ -156,9 +130,9 @@ public class AuthenticationController extends AbstractController {
 
   @Override
   public void initProperties(Properties properties) {
+    super.initProperties(properties);
     loginPage = (String) properties.get(LOGIN_PAGE_PROP);
-    this.authProvider = createAuthProvider(properties);
-    setupAuthentication(properties, authProvider);
+    setupAuthentication(properties, getAuthProvider());
     initUserSessionHandler();
     initLoginAction();
     initLogoutAction();
@@ -173,7 +147,7 @@ public class AuthenticationController extends AbstractController {
   }
 
   private void initUserSessionHandler() {
-    getNetRelay().getRouter().route().handler(UserSessionHandler.create(authProvider));
+    getNetRelay().getRouter().route().handler(UserSessionHandler.create(getAuthProvider()));
   }
 
   private void initLogoutAction() {
@@ -195,7 +169,7 @@ public class AuthenticationController extends AbstractController {
     String directLoginUrl = readProperty(DIRECT_LOGGED_IN_OK_URL_PROP, null, false);
     String authErrorParam = readProperty(AUTHENTICATION_ERROR_PARAM, null, false);
 
-    FormLoginHandlerBt fl = new FormLoginHandlerBt(authProvider);
+    FormLoginHandlerBt fl = new FormLoginHandlerBt(getAuthProvider());
     if (directLoginUrl != null) {
       fl.setDirectLoggedInOKURL(directLoginUrl);
     }
@@ -256,75 +230,6 @@ public class AuthenticationController extends AbstractController {
     default:
       throw new UnsupportedOperationException("unsupported definition for authentication handler: " + ae);
     }
-  }
-
-  private AuthProviderProxy createAuthProvider(Properties properties) {
-    String authProvider = (String) properties.get(AUTH_PROVIDER_PROP);
-    String mapper = readProperty(MongoAuth.PROPERTY_COLLECTION_NAME, null, true);
-    if (authProvider.equals(AUTH_PROVIDER_MONGO)) {
-      return new AuthProviderProxy(initMongoAuthProvider(mapper), mapper);
-    } else {
-      throw new UnsupportedOperationException("unsupported authprovider: " + authProvider);
-    }
-  }
-
-  /**
-   * Init the Authentication Service
-   */
-  private AuthProvider initMongoAuthProvider(String mapper) {
-    IDataStore store = getNetRelay().getDatastore();
-    if (!(store instanceof MongoDataStore)) {
-      throw new IllegalArgumentException("MongoAuthProvider expects a MongoDataStore");
-    }
-    JsonObject config = new JsonObject();
-    String saltStyle = readProperty(MongoAuth.PROPERTY_SALT_STYLE, HashSaltStyle.NO_SALT.toString(), false);
-    config.put(MongoAuth.PROPERTY_SALT_STYLE, HashSaltStyle.valueOf(saltStyle));
-    MongoAuth auth = MongoAuth.create(((MongoDataStore) store).getMongoClient(), config);
-
-    auth.setPasswordField(readProperty(MongoAuth.PROPERTY_PASSWORD_FIELD, null, true));
-    auth.setUsernameField(readProperty(MongoAuth.PROPERTY_USERNAME_FIELD, null, true));
-    auth.setCollectionName(mapper);
-
-    String roleField = readProperty(MongoAuth.PROPERTY_ROLE_FIELD, null, false);
-    if (roleField != null) {
-      auth.setRoleField(roleField);
-    }
-    String saltField = readProperty(MongoAuth.PROPERTY_SALT_FIELD, null, false);
-    if (saltField != null) {
-      auth.setSaltField(saltField);
-    }
-
-    return auth;
-  }
-
-  class AuthProviderProxy implements AuthProvider {
-    AuthProvider prov;
-    String mapper;
-
-    AuthProviderProxy(AuthProvider prov, String mapper) {
-      this.prov = prov;
-      this.mapper = mapper;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see io.vertx.ext.auth.AuthProvider#authenticate(io.vertx.core.json.JsonObject, io.vertx.core.Handler)
-     */
-    @Override
-    public void authenticate(JsonObject arg0, Handler<AsyncResult<User>> handler) {
-      prov.authenticate(arg0, result -> {
-        if (result.failed()) {
-          LOGGER.info("Authentication failed: " + result.cause());
-          handler.handle(result);
-        } else {
-          User user = result.result();
-          user.principal().put(MAPPERNAME_IN_PRINCIPAL, mapper);
-          handler.handle(Future.succeededFuture(user));
-        }
-      });
-    }
-
   }
 
   public enum AuthHandlerEnum {
