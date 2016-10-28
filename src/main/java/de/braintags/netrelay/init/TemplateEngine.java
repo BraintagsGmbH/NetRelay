b2fd17a2774d71ed70b701240f047c878aeff06a
Michael Remme <mremme@braintags.de>
2016-10-28 16:27:57 +0200

    enumeration fot template engines

---
+++ src/main/java/de/braintags/netrelay/init/TemplateEngine.java
@@ -0,0 +1,24 @@
+/*
+ * #%L
+ * NetRelay
+ * %%
+ * Copyright (C) 2016 Braintags GmbH
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
+package de.braintags.netrelay.init;
+
+/**
+ * Enumeration defined template engines, which are supported by NetRelay
+ * 
+ * @author Michael Remme
+ * 
+ */
+public enum TemplateEngine {
+
+  THYMELEAF;
+}
