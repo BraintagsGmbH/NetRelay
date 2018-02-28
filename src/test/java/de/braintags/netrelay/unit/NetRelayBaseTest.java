3a0df8b8b95e7e97fe94fddad61b87891b6e666d
Martin Pluecker <martin.pluecker@braintags.de>
2018-02-28 10:36:00 +0100

    fix shutdown

--- src/test/java/de/braintags/netrelay/unit/NetRelayBaseTest.java
+++ src/test/java/de/braintags/netrelay/unit/NetRelayBaseTest.java
@@ -39,6 +39,7 @@ import de.braintags.vertx.util.ResultObject;
 import de.braintags.vertx.util.exception.InitException;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
 import io.vertx.core.Vertx;
@@ -155,19 +156,26 @@ public abstract class NetRelayBaseTest {
   @AfterClass
   public static void shutdown(final TestContext context) throws Exception {
     LOGGER.debug("performing shutdown");
+    Future<Void> stopFuture = Future.future();
     if (netRelay != null) {
-      netRelay.stop();
+      netRelay.stop(stopFuture);
+    } else {
+      stopFuture.complete();
     }
     netRelay = null;
 
-    if (vertx != null) {
-      Async async = context.async();
-      vertx.close(ar -> {
-        vertx = null;
-        async.complete();
-      });
-      async.awaitSuccess();
-    }
+    Async async = context.async();
+    stopFuture.setHandler(v -> {
+      if (v.failed()) {
+        LOGGER.error("netrelay did not stop", v.cause());
+      }
+      if (vertx != null) {
+        vertx.close(ar -> {
+          vertx = null;
+          async.complete();
+        });
+      }
+    });
   }
 
   public KeyGeneratorVerticle createKeyGenerator(final TestContext context) {
