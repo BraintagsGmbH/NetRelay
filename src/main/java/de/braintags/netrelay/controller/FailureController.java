daf0001695f02655154a18ed28f6767571512f6d
Jan Kerkenhoff <jan.kerkenhoff@braintags.de>
2018-04-17 10:10:24 +0200

    fix failure controller

--- src/main/java/de/braintags/netrelay/controller/FailureController.java
+++ src/main/java/de/braintags/netrelay/controller/FailureController.java
@@ -19,10 +19,10 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
 
+import de.braintags.netrelay.routing.RouterDefinition;
 import de.braintags.vertx.util.ExceptionUtil;
 import de.braintags.vertx.util.exception.InitException;
 import de.braintags.vertx.util.request.RequestUtil;
-import de.braintags.netrelay.routing.RouterDefinition;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.ext.web.RoutingContext;
 
@@ -30,10 +30,10 @@ import io.vertx.ext.web.RoutingContext;
  * A Controller for failing calls. The Controller can be configured to produce output depending on an error code or an
  * exception. For each of them can be defined a redirect address. If no definition was found, then an internal default
  * output is generated. If no definition was found, then an internal default output is generated.
- * 
+ *
  * When this controller is added to a RouterDefinition, the {@link RouterDefinition#setFailureDefinition(boolean)}
  * should be set to true.
- * 
+ *
  * <br>
  * <br>
  * Config-Parameter:<br/>
@@ -42,31 +42,31 @@ import io.vertx.ext.web.RoutingContext;
  * <LI>Exception handling parameters<br>
  * All parameters, which are starting with {@value #EXCEPTION_START_PARAMETER} are interpreted as definition for
  * occuring exceptions. For example:
- * 
+ *
  * <pre>
  * EX:java.lang.IndexOutOfBoundsException=/error/exception.html
  * </pre>
- * 
+ *
  * The system tries to instantiate the defined Exception. If during runtime then an exception occures and the occured
  * Exception is the same or a subclass of the defined IndexOutOfBoundsException, then the redirect is processed to
  * the defined URI
  * <br>
- * 
+ *
  * <LI>Errorcode handling parameters<br>
  * All parameters, which are starting with {@value #ERRORCODE_START_PARAMETER} are interpreted for those situations,
  * where not an exception occured, but an errorcode was raised.
- * 
+ *
  * <LI>{@value #DEFAULT_PROPERTY}<br>
  * by using this propertyname, the default redirect address can be defined, which is used for the cases, where an
  * exception or an errorcode occured, where no definition could be found
  * </UL>
- * 
+ *
  * <br>
  * Request-Parameter:<br/>
  * <br/>
  * Result-Parameter:<br/>
  * <br/>
- * 
+ *
  * @author Michael Remme
  */
 public class FailureController extends AbstractController {
@@ -87,12 +87,12 @@ public class FailureController extends AbstractController {
    */
   public static final String DEFAULT_PROPERTY = "DEFAULT";
 
-  private Map<Class, String> exceptionDefinitions = new HashMap<>();
-  private Map<Integer, String> codeDefinitions = new HashMap<>();
+  private final Map<Class, String> exceptionDefinitions = new HashMap<>();
+  private final Map<Integer, String> codeDefinitions = new HashMap<>();
   private String defaultRedirect;
 
   @Override
-  public void initProperties(Properties properties) {
+  public void initProperties(final Properties properties) {
     LOGGER.info("init " + getClass().getName());
     defaultRedirect = readProperty(DEFAULT_PROPERTY, null, false);
     Enumeration<Object> keys = properties.keys();
@@ -107,7 +107,7 @@ public class FailureController extends AbstractController {
   }
 
   @SuppressWarnings("rawtypes")
-  private void addException(String key, String value) {
+  private void addException(final String key, final String value) {
     try {
       Class defClass = Class.forName(key.substring(EXCEPTION_START_PARAMETER.length()));
       exceptionDefinitions.put(defClass, value);
@@ -116,23 +116,27 @@ public class FailureController extends AbstractController {
     }
   }
 
-  private void addErrorCode(String key, String value) {
+  private void addErrorCode(final String key, final String value) {
     Integer code = Integer.parseInt(key.substring(ERRORCODE_START_PARAMETER.length()));
     codeDefinitions.put(code, value);
   }
 
+  @Override
+  protected void handleError(final RoutingContext context, final Throwable e) {
+    LOGGER.error("Exception during failure handling, something is really broken!", e);
+    if (!context.response().ended()) {
+      context.response().setStatusCode(HttpResponseStatus.INTERNAL_SERVER_ERROR.code());
+      context.response().end(context.response().getStatusMessage());
+    }
+  }
+
   /*
    * (non-Javadoc)
-   * 
+   *
    * @see io.vertx.core.Handler#handle(java.lang.Object)
    */
   @Override
-  public void handleController(RoutingContext context) {
-    HttpResponseStatus status = context.statusCode() > 0 ? HttpResponseStatus.valueOf(context.statusCode()) : null;
-
-    String reply = String.format("Statuscode %d %s for request %s", context.statusCode(),
-        status == null ? "" : "( " + status.reasonPhrase() + " )", context.request().path());
-    LOGGER.info(reply);
+  public void handleController(final RoutingContext context) {
     if (context.failure() != null) {
       reactByException(context);
     } else {
@@ -140,14 +144,15 @@ public class FailureController extends AbstractController {
     }
   }
 
-  private void reactByException(RoutingContext context) {
+  private void reactByException(final RoutingContext context) {
     Throwable error = context.failure();
     String redirect = getRedirectByException(error);
-    LOGGER.error("", error);
+    LOGGER.error("Exception during request " + context.request().absoluteURI(), error);
     if (redirect != null && !context.request().path().equalsIgnoreCase(redirect)) {
       RequestUtil.sendRedirect(context, redirect);
     } else {
-      String reply = String.format("Statuscode %d for request %s", context.statusCode(), context.request().path());
+      String reply = String.format("Statuscode %d for request %s", context.statusCode(),
+          context.request().absoluteURI());
       reply += "\n" + error.toString();
       reply += "\n" + ExceptionUtil.getStackTrace(error);
       if (!context.response().ended()) {
@@ -157,7 +162,7 @@ public class FailureController extends AbstractController {
   }
 
   @SuppressWarnings({ "rawtypes" })
-  private String getRedirectByException(Throwable error) {
+  private String getRedirectByException(final Throwable error) {
     Iterator<Entry<Class, String>> keys = exceptionDefinitions.entrySet().iterator();
     while (keys.hasNext()) {
       Entry<Class, String> entry = keys.next();
@@ -171,7 +176,11 @@ public class FailureController extends AbstractController {
     return null;
   }
 
-  private void reactByStatusCode(RoutingContext context) {
+  private void reactByStatusCode(final RoutingContext context) {
+    HttpResponseStatus status = context.statusCode() > 0 ? HttpResponseStatus.valueOf(context.statusCode()) : null;
+    String reply = String.format("Statuscode %d %s for request %s", context.statusCode(),
+        status == null ? "" : "( " + status.reasonPhrase() + " )", context.request().absoluteURI());
+    LOGGER.error(reply);
     String redirect = getRedirectByStatusCode(context.statusCode());
     if (redirect != null && !context.request().path().equalsIgnoreCase(redirect)) {
       RequestUtil.sendRedirect(context, redirect);
@@ -181,7 +190,7 @@ public class FailureController extends AbstractController {
 
   }
 
-  private String getRedirectByStatusCode(int statusCode) {
+  private String getRedirectByStatusCode(final int statusCode) {
     if (codeDefinitions.containsKey(statusCode)) {
       return codeDefinitions.get(statusCode);
     }
@@ -191,7 +200,7 @@ public class FailureController extends AbstractController {
     return null;
   }
 
-  private void handleDefaultStatus(RoutingContext context, String message) {
+  private void handleDefaultStatus(final RoutingContext context, final String message) {
     if (!context.response().ended()) {
       int code = context.statusCode() > 0 ? context.statusCode() : HttpResponseStatus.INTERNAL_SERVER_ERROR.code();
       context.response().setStatusCode(code);
@@ -201,7 +210,7 @@ public class FailureController extends AbstractController {
 
   /**
    * Creates a default definition for the current instance
-   * 
+   *
    * @return
    */
   public static RouterDefinition createDefaultRouterDefinition() {
